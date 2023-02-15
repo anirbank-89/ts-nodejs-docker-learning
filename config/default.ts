1bd740b6be3ea1f7867cd6cedc3ffccee80e38d6
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2023-02-15 11:11:13 +0530

    Database name change

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,9 @@
 export default {
   port: 1337,
-  dbUri: "mongodb://localhost:27017/rest-api-tutorial",
+  dbUri: 'mongodb://localhost:27017/ts_node',
   saltWorkFactor: 10,
-  accessTokenTtl: "15m",
-  refreshTokenTtl: "1y",
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
   accessTokenPrivateKey: ``,
   accessTokenPublicKey: ``,
   refreshTokenPrivateKey: ``,

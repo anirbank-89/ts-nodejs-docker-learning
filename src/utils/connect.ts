732e8cc30a98fcee5d0e417ebd6b9f7b4ec5c1e6
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2023-02-15 11:27:52 +0530

    Replace logger parameter 'prettyPrint'value  with pino-pretty transport

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-import config from "config";
-import logger from "./logger";
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger';
 
 async function connect() {
-  const dbUri = config.get<string>("dbUri");
+  const dbUri = config.get<string>('dbUri');
 
   try {
     await mongoose.connect(dbUri);
-    logger.info("DB connected");
-  } catch (error) {
-    logger.error("Could not connect to db");
+    logger.info('DB connected');
+  } catch (error: any) {
+    logger.error('Could not connect to db due to ', error);
     process.exit(1);
   }
 }
